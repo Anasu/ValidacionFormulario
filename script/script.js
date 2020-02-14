997d94665cc801631507dafdc119c40850771076
Ana <misushima@gmail.com>
2020-02-14 12:59:36 -0300

    Se capturan las variables

--- script/script.js
+++ script/script.js
@@ -1,6 +1,13 @@
 let validation = function () 
 {
   
+  var name = document.getElementById("fname").value;
+  var lastName = document.getElementById("lname").value;
+  var email = document.getElementById("email").value;
+  var user = document.getElementById("username").value;
+  var pass = document.getElementById("pass").value;
+  var phone = document.getElementById("phone").value;
+  
 
   event.preventDefault();
 }
